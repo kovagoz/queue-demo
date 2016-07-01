0d3126ddd0389c9ab3bb9748a6e680ba202007aa
Kovago Zoltan <kovagoz@444.hu>
2016-07-01 14:12:37 +0200

    Create the IoC container

---
+++ app/Container.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App;
+
+use Closure;
+use InvalidArgumentException;
+
+/**
+ * A simple IoC container.
+ */
+class Container
+{
+    /**
+     * Registered resolvers.
+     *
+     * @var array
+     */
+    protected $bindings = [];
+
+    /**
+     * Registered aliases.
+     *
+     * @var array
+     */
+    protected $aliases = [];
+
+    /**
+     * Bind a resolver to the container.
+     *
+     * @param string  $abstract
+     * @param Closure $resolver
+     * @return void
+     */
+    public function bind($abstract, Closure $resolver)
+    {
+        $this->bindings[$abstract] = $resolver;
+    }
+
+    /**
+     * Bind a resolver to the container what provides the same instance every time.
+     *
+     * @param string  $abstract
+     * @param Closure $resolver
+     * @return void
+     */
+    public function singleton($abstract, Closure $resolver)
+    {
+        $this->bindings[$abstract] = function () use ($abstract, $resolver) {
+            $instance = call_user_func($resolver, $this);
+
+            // Rebind the result to the same abstract.
+            $this->bindings[$abstract] = function () use ($instance) {
+                return $instance;
+            };
+
+            return $instance;
+        };
+    }
+
+    /**
+     * Bind a concrete instance to the container.
+     *
+     * @param string $abstract
+     * @param mixed  $instance
+     * @return void
+     */
+    public function instance($abstract, $instance)
+    {
+        $this->bindings[$abstract] = function () use ($instance) {
+            return $instance;
+        };
+    }
+
+    /**
+     * Register an alias for an abstract.
+     *
+     * @param string $abstract
+     * @param string $alias
+     * @return void
+     */
+    public function alias($abstract, $alias)
+    {
+        $this->aliases[$alias] = $abstract;
+    }
+
+    /**
+     * Make an instance.
+     *
+     * @param string $abstract
+     * @return mixed
+     */
+    public function make($abstract)
+    {
+        if (array_key_exists($abstract, $this->aliases)) {
+            $abstract = $this->aliases[$abstract];
+        }
+
+        if (!array_key_exists($abstract, $this->bindings)) {
+            throw new InvalidArgumentException;
+        }
+
+        if (is_callable($this->bindings[$abstract])) {
+            return call_user_func($this->bindings[$abstract], $this);
+        }
+
+        return $this->bindings[$abstract];
+    }
+}
---
+++ tests/ContainerTest.php
@@ -0,0 +1,100 @@
+<?php
+
+use App\Container;
+
+class ContainerTest extends PHPUnit_Framework_TestCase
+{
+    public function testResolveExistingBinding()
+    {
+        $container = new Container;
+
+        $container->bind('foo', function () {
+            return new stdClass;
+        });
+
+        $this->assertInstanceOf(stdClass::class, $container->make('foo'));
+    }
+
+    public function testResolvingTwiceProduceDifferentInstances()
+    {
+        $container = new Container;
+
+        $container->bind('foo', function () {
+            return new stdClass;
+        });
+
+        $first_instance = $container->make('foo');
+
+        $this->assertNotSame($first_instance, $container->make('foo'));
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testResolvingNonexistentBinding()
+    {
+        $container = new Container;
+
+        $container->make('foo');
+    }
+
+    /**
+     * @dataProvider singletonProvider
+     */
+    public function testResolvingSingleton($singleton)
+    {
+        $container = new Container;
+
+        $container->singleton('foo', function () use ($singleton) {
+            return $singleton;
+        });
+
+        $first_instance = $container->make('foo');
+
+        $this->assertSame($first_instance, $container->make('foo'));
+    }
+
+    public function singletonProvider()
+    {
+        return [
+            [new stdClass],
+            [function () {}]
+        ];
+    }
+
+    /**
+     * @dataProvider instanceProvider
+     */
+    public function testBindInstance($instance)
+    {
+        $container = new Container;
+
+        $container->instance('foo', $instance);
+
+        $this->assertSame($instance, $container->make('foo'));
+    }
+
+    public function instanceProvider()
+    {
+        return [
+            [new stdClass],
+            [function () {}]
+        ];
+    }
+
+    public function testUseAlias()
+    {
+        $container = new Container;
+
+        $container->bind('foo', function () {
+            return new stdClass;
+        });
+
+        $container->alias('foo', 'bar');
+
+        $this->assertEquals(
+            $container->make('foo'),
+            $container->make('bar')
+        );
+    }
+}
