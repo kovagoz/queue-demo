a28e4ab1e3944b73c8c682c667b8aa50c77cd2eb
Zoltan Kovago <kovago@gmail.com>
2016-07-05 00:29:23 +0200

    Log controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,7 @@
 <?php
 
-$router->post('hello', function () {
-    return [
-        'created_at' => time(),
-        'message'    => 'Lorem ipsum dolor sit amet'
-    ];
-});
-
 $router->get('log', function ($request, $app) {
-    return $app('db')->execute('SELECT * FROM log ORDER BY created_at DESC');
+    return $app('audit')
+        ->findSince($request->getParam('since'))
+        ->toArray();
 });
