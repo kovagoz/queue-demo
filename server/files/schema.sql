714b091e762217b870b684d668911f7d21d62615
Zoltan Kovago <kovago@gmail.com>
2016-07-04 22:30:05 +0200

    Modify the database schema and fix the Ansible playbook

--- server/files/schema.sql
+++ server/files/schema.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS log;
 
 CREATE TABLE log (
     id serial,
-    ts timestamp NOT NULL,
+    created_at timestamp NOT NULL,
     severity varchar(10) NOT NULL,
-    msg TEXT,
+    message TEXT,
     PRIMARY KEY (id)
 );
