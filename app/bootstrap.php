eed7c24a9cbf9fc639c1c3625f7add2da1a574d9
Zoltan Kovago <kovago@gmail.com>
2016-07-05 23:46:41 +0200

    Some cosmetic changes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\Application;
 
+//---------------------------------------------------------
+//  PHP settings
+//---------------------------------------------------------
+
 setlocale(LC_ALL, 'hu_HU.UTF-8');
 
 date_default_timezone_set('Europe/Budapest');
@@ -11,5 +15,9 @@ date_default_timezone_set('Europe/Budapest');
 error_reporting(E_ALL);
 
 ini_set('display_errors', 1);
+
+//---------------------------------------------------------
+//  Create and boot the application
+//---------------------------------------------------------
 
 $app = (new Application)->boot();
