1f942428e2734f1f769d7c5cc36b23d66bfc4415
Kovago Zoltan <kovagoz@444.hu>
2016-07-05 17:30:24 +0200

    Refactor Http\Response

--- app/Http/Response.php
+++ app/Http/Response.php
@@ -84,17 +84,56 @@ class Response implements ResponseContract
             $this->setStatus(204);
         }
 
-        http_response_code($this->status ?: 200);
+        $this->setFinalStatusCode();
 
         if (is_array($this->body)) {
-            $this->setBody(json_encode($this->body));
-            $this->setHeader('Content-Type', 'application/json');
+            $this->setJsonResponse();
         }
 
+        $this->sendHeaders();
+        $this->sendBody();
+    }
+
+    /**
+     * Set body and headers for a JSON response.
+     *
+     * @return void
+     */
+    protected function setJsonResponse()
+    {
+        $this->setBody(json_encode($this->body));
+        $this->setHeader('Content-Type', 'application/json');
+    }
+
+    /**
+     * Set the HTTP status code will be sent.
+     *
+     * @return void
+     */
+    protected function setFinalStatusCode()
+    {
+        http_response_code($this->status ?: 200);
+    }
+
+    /**
+     * Send HTTP headers to the client.
+     *
+     * @return void
+     */
+    protected function sendHeaders()
+    {
         foreach ($this->headers as $name => $value) {
             header("{$name}: {$value}", true);
         }
+    }
 
+    /**
+     * Send the response body if not empty.
+     *
+     * @return void
+     */
+    protected function sendBody()
+    {
         if ($this->body !== null) {
             echo $this->body;
         }
