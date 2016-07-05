a278a4359a85c651eb977c18cc4e910d913a0c5c
Zoltan Kovago <kovago@gmail.com>
2016-07-05 08:41:05 +0200

    New worker

--- worker.php
+++ worker.php
@@ -2,19 +2,6 @@
 
 require_once __DIR__ . '/app/bootstrap.php';
 
-use App\Queue\Message;
+use App\Domain\MessageProcessor;
 
-$app('queue')->listen(function (Message $message) {
-    if (rand(0, 3)) {
-        echo 'FAILED', PHP_EOL;
-        if ($message->isDying()) {
-            $message->done();
-            echo 'JOB REMOVED', PHP_EOL;
-        } else {
-            $message->reject();
-        }
-    } else {
-        $message->done();
-        echo '[x] ', $message->getPayload(), PHP_EOL;
-    }
-});
+$app(MessageProcessor::class)->start();
